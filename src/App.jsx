125a0c5f64cee1cc3c853d9303ebe8eb0a7e39f2
Danylo Musiiaka <dollarone98@gmail.com>
2023-11-15 11:44:32 +0200

    upd structure to react vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+import WelcomePage from './components/WelcomePage'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <WelcomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header class="navbar navbar-light bg-light mx-4">
+            <a href="#" class="navbar-brand h1">Yukis</a>
+            <div class="form-inline">
+                <button class="btn login mx-2">Log in</button>
+                <button class="btn sign-in">Sign up</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
